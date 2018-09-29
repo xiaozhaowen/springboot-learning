ee87cab7d41f526d04e8db74e5c6523d0f020710
xiaozhao <ab055089wuxiaozhao@ab-insurance.com>
2018-09-29 09:33:27 +0800

    Initial commit

---
+++ src/main/java/com/xiaozhao/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.xiaozhao.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xiaozhao
+ * @date 2018/9/29上午9:31
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping()
+    public String hello() {
+        return "Hello SpringBoot";
+    }
+}
