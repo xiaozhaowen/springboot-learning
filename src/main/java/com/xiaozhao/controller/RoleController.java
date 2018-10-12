2d1ee310c49f407995a15d04a8a0f43080d12653
xiaozhao <ab055089wuxiaozhao@ab-insurance.com>
2018-10-12 09:56:59 +0800

    logback简答集成

--- src/main/java/com/xiaozhao/controller/RoleController.java
+++ src/main/java/com/xiaozhao/controller/RoleController.java
@@ -2,6 +2,8 @@ package com.xiaozhao.controller;
 
 import com.xiaozhao.domain.Role;
 import com.xiaozhao.service.RoleService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +19,15 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/role")
 public class RoleController {
+
+    private static final Logger logger = LoggerFactory.getLogger(RoleController.class);
+
     @Autowired
     private RoleService roleService;
 
     @RequestMapping("/list")
     public List<Role> queryAll() {
+        logger.info("查询所有的角色");
         return this.roleService.queryAll();
     }
 
--- src/test/java/com/xiaozhao/SpringbootLearningApplicationTests.java
+++ src/test/java/com/xiaozhao/SpringbootLearningApplicationTests.java
@@ -1,16 +1,37 @@
 package com.xiaozhao;
 
+import com.xiaozhao.controller.HelloController;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootLearningApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+//    private MockMvc mvc;
+
+    @Before
+    public void setUp() throws Exception {
+//        mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    }
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+    public void getHello()  throws Exception{
+
+    }
 
 }
